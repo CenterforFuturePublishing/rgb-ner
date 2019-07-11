819ec22feb5747c7d4f6b70d2d9c47f68530a59a
azertypow <azertypow@gmail.com>
2019-07-11 16:34:14 +0200

    feat: init column

---
+++ src/components/Column.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="v-column">
+        <button @click="buttonClicked">click</button>
+        <p>{{counter}}</p>
+    </section>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+
+    @Component
+    export default class Column extends Vue {
+
+      counter = 0
+
+      buttonClicked() {
+        this.counter ++
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .v-column {
+
+    }
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Coucou Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
@@ -52,6 +52,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: red;
 }
 </style>
