f665d6acd2e549e74f00c1883651a16dbf8673a3
azertypow <azertypow@gmail.com>
2019-07-12 16:32:37 +0200

    feat: add column and app style

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
 
+    <main class="v-container">
 
-    <column :textFromParent="textForChildComponent">
+      <column></column>
 
-      <h4>html from parent</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem et harum, hic, illo ipsam iure, libero officia possimus quae quam quo recusandae sed unde voluptate. Asperiores non reprehenderit soluta.</p>
-
-    </column>
-
-
+    </main>
 
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 import Column from "@/components/Column.vue"
 
 @Component({
@@ -24,18 +19,26 @@ import Column from "@/components/Column.vue"
     Column,
   },
 })
-export default class App extends Vue {
-  textForChildComponent = 'hello from parrent component'
-}
+export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    box-sizing: border-box;
+    width: 100vw;
+    height: 100vh;
+    padding: 10rem 2rem 2rem;
+  }
+
+  .v-container {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    display: flex;
+  }
+
 </style>
