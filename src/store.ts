09e923bc98973a4da6f12571ea5d4aa353a985aa
azertypow <azertypow@gmail.com>
2019-07-12 16:30:15 +0200

    feat: load data api to global store

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {getJpegData, IApi} from "@/api"
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
 
+export const store = new Vuex.Store<IRootState>({
+  state: {
+    jpegData: null
   },
   mutations: {
-
+    updateJpegData(state, jpegData: IApi) {
+      state.jpegData = jpegData
+    }
   },
   actions: {
 
   }
 })
+
+getJpegData().then(value => {
+  store.commit("updateJpegData", value)
+})
+
+export interface IRootState {
+  jpegData: IApi | null
+}
